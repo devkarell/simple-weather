2818b1abede8b70b695cea18f43349c50baa7895
Karell Lutkmeier <kareudeveloper@gmail.com>
2025-04-12 19:01:09 -0300

    init project

---
+++ src/App.ts
@@ -0,0 +1,8 @@
+import './styles/global.scss';
+// import * as provincesFiller from './modules/provincesFiller.ts';
+// import * as regionValidator from './modules/regionValidator.ts';
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     regionValidator.init();
+//     provincesFiller.fill();
+// });
